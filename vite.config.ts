21b942c2ddeb8e331a67ea99aae2e36bc8b6f870
Rekha <stackgurusolutions@gmail.com>
2026-05-10 10:59:35 +0530

    Fixed Vercel frontend deployment

--- vite.config.ts
+++ vite.config.ts
@@ -12,26 +12,35 @@ export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    ...process.env.NODE_ENV !== "production" && process.env.REPL_ID !== void 0
-      ? [await import("@replit/vite-plugin-cartographer").then(m => m.cartographer())]
-      : []
+    ...process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== void 0
+      ? [
+          await import("@replit/vite-plugin-cartographer").then((m) =>
+            m.cartographer()
+          ),
+        ]
+      : [],
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
       "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets")
-    }
+      "@assets": path.resolve(__dirname, "attached_assets"),
+    },
   },
+
   root: path.resolve(__dirname, "client"),
+
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
   },
+
   server: {
     fs: {
       strict: true,
-      deny: ["**/.*"]
-    }
-  }
+      deny: ["**/.*"],
+    },
+  },
 });

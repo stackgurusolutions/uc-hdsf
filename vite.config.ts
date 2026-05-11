6c5dabdfef00f0f752a2cb15132ff2be2b38dc7c
Rekha <stackgurusolutions@gmail.com>
2026-05-11 09:22:31 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -12,25 +12,15 @@ export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== void 0
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer()
-          ),
-        ]
-      : []),
   ],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
+      "@": path.resolve(__dirname, "src"),
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-
-  root: path.resolve(__dirname, "client"),
 
   build: {
     outDir: path.resolve(__dirname, "dist"),

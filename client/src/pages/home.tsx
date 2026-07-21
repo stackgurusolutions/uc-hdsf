c14bedc36491087cde8fadda6c69fa619614d39d
Mahesh Jangid <seelak123@gmail.com>
2026-07-21 10:21:24 +0530

    updated

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -470,7 +470,7 @@ export default function Home() {
               {/* Keynote Speakers */}
               <div>
                 <h2 className="text-3xl font-bold text-center mb-12">
-                  Keynote Speakers(To be Decided)
+                  Keynote Speakers
                 </h2>
 
                  <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
